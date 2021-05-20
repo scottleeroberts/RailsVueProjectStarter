e8d762c46bffad05731bda52eaba8bd9e9336cbf
Keith Thibodeaux <keith@railscoder.net>
2021-05-20 09:30:19 -0400

    Remove unnecessary axios setting

--- frontend/src/store/modules/auth.js
+++ frontend/src/store/modules/auth.js
@@ -49,7 +49,6 @@ export default {
     setUserData(state, userData) {
       state.user = userData
       localStorage.setItem('user', JSON.stringify(userData))
-      axios.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`
     },
     clearUserData() {
       localStorage.removeItem('user')
